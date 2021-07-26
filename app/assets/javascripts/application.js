83708f065077f1f214aa55301c807336ce721d9b
Leandro <leandro83g@icloud.com>
2021-07-26 18:38:21 +0900

    slick setting done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,11 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
+$(function() {
+    $('.single-item').slick({
+          infinite: true,
+          dots:true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+     });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root 'home#top'
+  get 'home/about'
 	devise_for :users
   resources :users,only: [:show,:edit,:update,:index]
   resources :books
-  root 'home#top'
-  get 'home/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
