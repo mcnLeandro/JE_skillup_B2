09fcc3840e1bea68892c1907c4b60ed5095f9bfc
Leandro <leandro83g@icloud.com>
2021-07-26 18:56:42 +0900

    jpostal setting done

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,23 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+          :recoverable, :rememberable, :trackable, :validatable
+
   attachment :profile_image, destroy: false
+
   has_many :books
   validates :name, presence: true, length: {maximum: 10, minimum: 2}
   validates :introduction, length: {maximum: 50}
+
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+
+  def prefecture_name
+      JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+
+  def prefecture_name=(prefecture_name)
+      self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
 
 end 
